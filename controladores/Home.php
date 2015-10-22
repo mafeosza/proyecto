ceeae0ef52dfd33c05e692edd6cf05852239bdaa
Maria Fernanda <fercha0202@hotmail.com>
2015-10-21 19:49:38 -0500

    index htacces

---
+++ controladores/Home.php
@@ -0,0 +1,57 @@
+<?php
+ /**
+ * 
+ */
+ require_once("libs/Controlador.php");
+ class Home extends Controlador
+ {
+ 
+ 	public function imprimir()
+ 	{
+			print_r($this->parametros);
+	}
+
+ 	public function index()
+ 	{
+		$usuario=$this->cargarModelo("Usuario");
+		$this->cargarVista("index");
+		
+	}
+	public function register() 
+	{
+		$nombre = $_POST ["nombre"];
+		$username = $_POST ["username"];
+		$pass = $_POST ["pass"];
+		$pass2 = $_POST ["pass2"];
+		$modelo = $this->cargarModelo ( "Usuario" );
+		$result = $modelo->insertUser ( $nombre, $username, $pass, $pass2 );
+		
+		echo "El usuario fue registrado con exito";
+		echo "<hr/>";
+	}
+	public function login()
+	{
+		$username = $_POST ["username"];
+		$password = $_POST ["pass"];
+		
+		$modelo = $this->cargarModelo ( "Usuario" );
+		$respuesta = $modelo->authenticate ( $username, $password );
+		if ($respuesta != null && $respuesta->rowCount () > 0) {
+			setcookie ( "chsm", "logedin", time () + 3600, "/" );
+			
+			header ( "location: /proyecto" );
+			exit ();
+			
+		} else {
+			echo "login fallo";
+			$this->cargarVista ( "index" );
+		}
+	}
+	public function logout() 
+	{
+		setcookie ( "chsm", "", time () - 3600, "/" );
+		header ( "Location: /proyecto" );
+	}
+
+ }
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+	class FrontController{
+		private $controlador="home";
+		private $metodo="index";
+		private $params;
+
+
+	public function index(){
+
+		$url = $_SERVER["REQUEST_URI"];
+   		$path = trim(parse_url($url, PHP_URL_PATH), "/");
+   		try{
+   			@list($appname,$controlador, $metodo, $params)=explode("/",$path,4);
+   			@$params=(explode("/", $params));
+
+   		if ($controlador==null) {
+
+   			$controlador=$this->controlador;
+   		}
+
+   		if($metodo==null){
+			$metodo=$this->metodo;
+   		}
+   		$micontrolador=$this->cargarContolador($controlador);
+   		if ($micontrolador!=null) {
+   			
+   			$micontrolador->setParametros($params);
+   			$stringMetodo=$metodo;
+
+
+   			if (method_exists($micontrolador,$stringMetodo)) {
+   				
+   				$micontrolador->$stringMetodo();
+   			}else{
+   				echo "metodo no existe";
+   			}
+   		}else {
+   			
+   			echo "El controlador no existe";
+   		}
+   	}catch (Exception $e){
+
+   		echo $e->getMessage();
+   	}
+
+
+	}
+
+	function cargarContolador($controlador){
+
+		$controlador=ucfirst(strtolower($controlador));
+		$urlFile='controladores/' . $controlador . '.php';
+		if (file_exists($urlFile)) {
+			# code...
+			include $urlFile;
+			$class = $controlador;
+			$controller=new $class();
+			return $controller;
+		}else{
+			return null;
+		}
+	}
+
+
+	public function getControlador(){
+		return $this->controlador;
+	}
+
+	public function setControlador($controlador){
+		$this->controlador=$controlador;
+	}
+
+	public function getMetodo(){
+		return $this->metodo;
+	}
+
+	public function setMetodo($metodo){
+		$this->metodo=$metodo;
+	}
+
+	public function getParams(){
+		return $this->params;
+	}
+
+	public function setParams($params){
+		$this->params=$params;
+	}
+}
+
+$FrontController= new FrontController();
+$FrontController->index();
+
+?>
